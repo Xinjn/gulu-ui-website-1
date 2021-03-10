0607d132976765a502dd93a75957b9bd0e42794b
Xinjn <422075616@qq.com>
2021-03-10 23:21:32 +0800

    优化 css 代码

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -22,33 +22,26 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  $h: 22px;
-  $h2: $h - 4px;
-  button{
-    height: $h;
-    width: $h*2;
-    border: none;
-    background: grey;
-    border-radius: $h/2;
-    position: relative;
+
+$h: 22px;
+$h2: $h - 4px;
+button {
+  height: $h; width: $h * 2; border: none; background: #bfbfbf; border-radius: $h/2; position: relative;
+  > span {
+    position: absolute; top: 2px; left: 2px; height: $h2; width: $h2; background: white; border-radius: $h2 / 2; transition: all 250ms;
   }
-  span{
-    position: absolute;
-    top: 2px;
-    left: 2px;
-    height: $h2;
-    width: $h2;
-    background:white;
-    border-radius: $h2 / 2;
-    transition: left 250ms;
+
+  &.checked { background: #1890ff;
+    > span { left: calc(100% - #{$h2} - 2px); }
   }
-  button.checked{
-      background: blue;
+
+  &:focus { outline: none; }
+  &:active {
+    > span { width: $h2 + 4px; }
   }
-  button.checked > span {
-      left:calc(100% - #{$h2} - 2px);
+
+  &.checked:active {
+    > span { width: $h2 + 4px; margin-left: -4px; }
   }
-  button:focus{
-      outline: none;
-  }
+}
 </style>
