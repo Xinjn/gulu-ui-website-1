32892b925ee7563f8a3fbabb41bdaab8d669cb0e
Xinjn <422075616@qq.com>
2021-03-13 00:32:32 +0800

    加载中 css

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,6 +1,7 @@
 <template>  
         <button class="gule-button" :class="classes" 
         :disabled="disabled">
+        <span v-if="loading" class="gulu-loadingIndicator"></span>
             <slot/>
         </button>
 </template>
@@ -22,7 +23,11 @@ export default {
         },
         disabled:{
             type:Boolean,
-            default:'false'
+            default:false
+        },
+        loading:{
+            type:Boolean,
+            default:false
         }
     },
     setup(props){
@@ -164,5 +169,20 @@ export default {
                 color: $grey;
                 }
             }
+            > .gulu-loadingIndicator{
+                width: 14px;
+                height: 14px;
+                display: inline-block;
+                margin-right: 4px;
+                border-radius: 8px; 
+                border-color: $blue $blue $blue transparent;
+                border-style: solid;
+                border-width: 2px;
+                animation: gulu-spin 1s infinite linear;
+            }
+            }
+            @keyframes gulu-spin {
+            0%{transform: rotate(0deg)} 
+            100%{transform: rotate(360deg)} 
     }
 </style>
