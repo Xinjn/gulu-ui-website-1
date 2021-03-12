c11889ae0d65fa3ffd76d4fa5f56652a66a19c5a
Xinjn <422075616@qq.com>
2021-03-12 23:42:12 +0800

    创建 button 组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -4,7 +4,10 @@
     </div>
     <h1>示例1</h1>
     <div>
-        <Button>
+        <Button @click="onClick"
+        @focus="onClick"
+        @mouseover="onClick"
+        >
             你好
         </Button>
     </div>
@@ -12,6 +15,12 @@
 <script lang="ts">
 import Button from '../lib/Button.vue'
 export default {
-    components:{Button}
+    components:{Button},
+    setup(){
+        const onClick = () => {
+            console.log('hi')
+        }
+        return {onClick}
+    }
 }
 </script>
