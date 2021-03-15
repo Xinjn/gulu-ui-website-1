71d7073297904e7fe62c33c18b8bac6741a6c0ab
Xinjn <422075616@qq.com>
2021-03-15 21:16:46 +0800

    使用本地的svg.jssvg.js

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,28 @@
 <template>
-<div class="topnavAndBanner">
-    <Topnav/>
-    <div class="banner">
-        <h1>轱辘UI</h1>
-        <h2>一个厉害的UI框架</h2>
-        <p class="actions">
-            <a href="http://github.com">GitHub</a>
-            <router-link to="/doc">开始</router-link>
-        </p>
-    </div>
+<div>
+  <div class="topnavAndBanner">
+      <Topnav/>
+      <div class="banner">
+          <h1>轱辘UI</h1>
+          <h2>一个厉害的UI框架</h2>
+          <p class="actions">
+              <a href="http://github.com">GitHub</a>
+              <router-link to="/doc">开始</router-link>
+          </p>
+      </div>
+  </div>
+  <div class="features">
+    <svg class="icon">
+        <use xlink:href="#icon-vue"></use>
+    </svg>
+    <svg class="icon">
+        <use xlink:href="#icon-ts"></use>
+    </svg>
+    <svg class="icon">
+        <use xlink:href="#icon-light"></use>
+    </svg>
+  </div>
 </div>
-    
 </template>
 
 <script lant="ts">
@@ -21,27 +33,39 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$green:#02bcb0;
+$border-radius:4px;
+$color:#007974;
 .topnavAndBanner{
   background: linear-gradient(90deg, rgba(227,255,253,1) 0%, rgba(183,233,230,1) 100%);
 }
+
+.features{
+  >svg {
+    width: 64px;
+    height: 64px;
+  }
+}
    .banner {
-  padding: 100px 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+      color:$color;
+      padding: 100px 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
 
   > .actions{
     padding: 8px 0;
     a {
       margin:0 8px;
-      background: #fff;
-      display: inline-block;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 8px;
+      background: $green;
+      color: white;
+      display: inline-block; 
+      border-radius:$border-radius;
+      padding: 8px 24px;
+      &:hover {
+        text-decoration: none;
+      }
     }
   }
 }
