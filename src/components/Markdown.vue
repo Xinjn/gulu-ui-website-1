8642c3b67cfa95b5f6e7d8091ea9b2222295167e
Xinjn <422075616@qq.com>
2021-03-16 00:36:18 +0800

    事不过三：优化markdown

---
+++ src/components/Markdown.vue
@@ -0,0 +1,28 @@
+<template>
+<article class="markdown-body" 
+v-html="content">
+</article>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue'
+export default {
+    props:{
+        path:{
+            type:String,
+            required:true
+        }
+    },
+    setup(props){
+        const content = ref<string>(null)
+        import(props.path).then(result =>{
+            setTimeout(()=>{
+                content.value = result.default
+            },1000)
+            
+        })
+        return {content}
+    }
+
+}
+</script>
