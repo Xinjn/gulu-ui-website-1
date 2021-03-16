e27dd90534891ac0f099a81f27a4f79c7a5a6c2d
Xinjn <422075616@qq.com>
2021-03-16 21:32:53 +0800

    BUG：不展示源代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -11,7 +11,7 @@
       <Button>查看代码</Button>
     </div>
     <div class="demo-code">
-      <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+      <pre>{{Switch1Demo.__sourceCode}}</pre>
     </div>
   </div>
   <div class="demo">
@@ -23,7 +23,7 @@
       <Button>查看代码</Button>
     </div>
     <div class="demo-code">
-      <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
+      <pre>{{Switch2Demo.__sourceCode}}</pre>
     </div>
   </div>
 </div>
@@ -37,6 +37,7 @@ import {
 } from 'vue'
 import Switch1Demo from '../components/Switch1.demo.vue'
 import Switch2Demo from '../components/Switch2.demo.vue'
+console.log(Switch1Demo.__sourceCode)
 export default {
     components:{Switch,Button,Switch1Demo,Switch2Demo},
     setup(){
