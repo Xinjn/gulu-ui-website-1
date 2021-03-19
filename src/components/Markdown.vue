a33e2bf9224ce5467ababba26670b228226f06ce
Xinjn <422075616@qq.com>
2021-03-19 23:58:44 +0800

    解决上节课的遗留问题

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -8,21 +8,10 @@ v-html="content">
 import { ref } from 'vue'
 export default {
     props:{
-        path:{
+        content:{
             type:String,
             required:true
         }
     },
-    setup(props){
-        const content = ref<string>(null)
-        import(props.path).then(result =>{
-            setTimeout(()=>{
-                content.value = result.default
-            },1000)
-            
-        })
-        return {content}
-    }
-
 }
 </script>
